1cd30f1574f5fba1ea44c52865321692f6754894
Nate Beatty <nate.beatty@gmail.com>
2016-09-23 08:29:33 -0400

    Fixed errant char

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ function syncIt() {
 		--exclude "README.md" \
 		--exclude "LICENSE.txt" \
 		--exclude "archive" \
-		-avh --no-perms . ~;s 
+		-avh --no-perms . ~;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
